5ecee585273d339f966408635a87f7bc0e34c5c7
ayush-1601 <bansalayush1601@gmail.com>
2022-11-02 19:21:15 +0530

    generator page ui

--- lib/generaterPage.dart
+++ lib/generaterPage.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'dart:io';
 import 'package:flutter/rendering.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_qr_scanner/homePage.dart';
@@ -22,12 +23,26 @@ class _GeneratePageState extends State<GeneratePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('QR code generator'),
-        actions: <Widget>[],
+        centerTitle: true,
+        title: Text(
+          "QR Code Generator",
+          style: GoogleFonts.lato(
+            fontSize: 25,
+          ),
+        ),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back_ios_sharp)),
+        backgroundColor: Color(0xfff289ac9),
       ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Color(0xfffdff3f8),
             padding: const EdgeInsets.all(20.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -36,55 +51,91 @@ class _GeneratePageState extends State<GeneratePage> {
                 RepaintBoundary(
                   key: globalKey,
                   child: Container(
+                    alignment: Alignment.center,
                     // color: Colors.white,
                     decoration: BoxDecoration(
-                      border: Border.all(color: Colors.red),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    height: 350, width: 350,
+                        color: Color(0xfffdff3f8),
+                        border: Border.all(color: Color(0xfffdff3f8), width: 5),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xfff289ac9),
+                            offset: const Offset(0.0, 0.0),
+                            blurRadius: 20,
+                            spreadRadius: 5.0,
+                          )
+                        ]),
+                    height: 300, width: 300,
                     // padding: EdgeInsets.all(8.0),
                     child: QrImage(
                       data: qrData,
-                      padding: EdgeInsets.all(40.0),
+                      padding: const EdgeInsets.all(20.0),
                     ),
                   ),
                 ),
                 SizedBox(height: 50),
-                const Text(
-                  "New QR Link Generator", style: TextStyle(fontSize: 20),
+                Text(
+                  "Generate new QR code",
+                  style: GoogleFonts.lato(fontSize: 20),
                 ),
-                SizedBox(height: 20,),
+                SizedBox(
+                  height: 20,
+                ),
                 TextField(
+                  cursorColor: Color(0xfff289ac9),
                   controller: qrdataFeed,
-                  decoration: const InputDecoration(
-                    label: Text("input ur link here"),
+                  decoration: InputDecoration(
+                    label: Text(
+                      "Input your link/text here",
+                      style: GoogleFonts.lato(color: Color(0xfff289ac9)),
+                    ),
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(50, 100, 50, 50),
                   child: Column(
                     children: [
-                      FloatingActionButton.extended(
-                        // paddding: const EdgeInsets.all(15.0),
-                        onPressed: () {
-                          if (qrdataFeed.text.isEmpty) {
-                            setState(() {
-                              qrData = "";
-                            });
-                          } else {
-                            setState(() {
-                              qrData = qrdataFeed.text;
-                            });
-                          }
-                        },
-                        label: const Text("Generate QR"),
+                      SizedBox(
+                        width: 200,
+                        // height: 30,
+                        child: FloatingActionButton.extended(
+                          backgroundColor: Color(0xfff289ac9),
+                          shape: StadiumBorder(),
+                          // paddding: const EdgeInsets.all(15.0),
+                          onPressed: () {
+                            if (qrdataFeed.text.isEmpty) {
+                              setState(() {
+                                qrData = "";
+                              });
+                            } else {
+                              setState(() {
+                                qrData = qrdataFeed.text;
+                              });
+                            }
+                          },
+                          label: Text(
+                            "Generate QR".toUpperCase(),
+                            style: GoogleFonts.lato(fontSize: 18),
+                          ),
+                        ),
                       ),
                       const SizedBox(
-                        height: 10.0,
+                        height: 15.0,
                       ),
-                      FloatingActionButton.extended(
-                        onPressed: _shareqr,
-                        label: const Text("Share"),
+                      SizedBox(
+                        // height: 30,
+                        width: 200,
+                        child: FloatingActionButton.extended(
+                          backgroundColor: Color(0xfff289ac9),
+                          shape: const StadiumBorder(),
+                          onPressed: _shareqr,
+                          label: Text(
+                            "Share".toUpperCase(),
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
                       )
                     ],
                   ),
